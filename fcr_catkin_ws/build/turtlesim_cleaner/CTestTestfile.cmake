0d8e369c266c0981badf83897df173d811c49d57
abdullah-zaiter <abdullah.zaiter@hotmail.com>
2017-08-03 01:49:38 -0300

    corrigindo besteiras

--- fcr_catkin_ws/build/turtlesim_cleaner/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/abdullah/fcr_catkin_ws/src/turtlesim_cleaner
-# Build directory: /home/abdullah/fcr_catkin_ws/build/turtlesim_cleaner
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
